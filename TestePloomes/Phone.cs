22fd47d7d028e5ca10462b4b6d6797814623b8f1
VinnyDSR <r.viniciusds@gmail.com>
2020-10-26 13:01:00 -0300

    Revert "Initial commit"
    This reverts commit 46f8e7a2892b9d8e95fd35b0c569269e18652b4f.

--- TestePloomes/Phone.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestePloomes
-{
-    public class Phone
-    {
-        public string PhoneNumber { get; set; }
-        public int TypeId { get; set; }
-        public int CountryId { get; set; }
-    }
-}
